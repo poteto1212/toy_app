3a7869f8ba2b9ca981daf100f8e4370215ba91f1
Your Name <you@example.com>
2021-06-26 14:12:17 +0000

    First toy app

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,5 +2,5 @@ class Micropost < ApplicationRecord
     #外部キーの指定
     belongs_to:user;
     #バリデーション設定
-    validates :content, length:{maximum:140}
+    validates :content, length:{maximum:140},presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     #n個の外部キーを認める
     has_many:microposts;
+    
+    #バリデーション設定
+    validates :name, presence: true    # 「FILL_IN」をコードに置き換えてください
+    validates :email, presence: true    # 「FILL_IN」をコードに置き換えてください
 end
