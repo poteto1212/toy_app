ffab7c4a41fc6a7b4a1ea62e2cc8ef6c60dff32c
Your Name <you@example.com>
2021-06-26 10:34:31 +0000

    init

---
+++ app/models/micropost.rb
@@ -0,0 +1,6 @@
+class Micropost < ApplicationRecord
+    #外部キーの指定
+    belongs_to:user;
+    #バリデーション設定
+    validates :content, length:{maximum:140}
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    #n個の外部キーを認める
+    has_many:microposts;
 end
